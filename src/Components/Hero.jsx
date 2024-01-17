a11ecc0b86731d25606d9d2cbd54c62b8cd0987a
JeanCarlosPerez <jeancperez09@gmail.com>
2024-01-17 10:42:27 +0100

    add hero and image

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Hero() {
+    return(
+        <section>
+            <img src="" alt="Geranio muy grande" />
+            <div>
+            <a class="gigante">GE<br/>RA   <br/>NIUM</a>
+            <p class="descripcionGeranio__hero">El género Geranium, comúnmente llamado geranio, agrupa 422 especies de plantas anuales, bienales y perennes frecuentemente utilizadas en jardinería por sus atractivas flores y su aroma característico</p>
+            </div>
+        </section>
+
+        
+    )
+}
+
+export default Hero;
