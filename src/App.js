e5357616e841cdaeac6fc8ef1a338ca8ce3ccdd0
FiorellaSF <afiosf@gmail.com>
2024-01-18 14:37:26 +0100

    Add Modal

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './Components/header/Header.css';
 import React, { useState, useEffect } from 'react';
 import Pagination from './Pagination';
 import './pagination.css'
+import './ProductModal.css'
+import ProductModal from './ProductModal';
 // import NavigationBar from './NavegationBar';
 // import ProductListFiltered from './ProductListFiltered';
 
