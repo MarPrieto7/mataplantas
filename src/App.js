4919101612ef7219c355d004caa5be4a89283aa4
FiorellaSF <afiosf@gmail.com>
2024-01-18 10:56:03 +0100

    Add Pagination

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import Footer from './Components/footer/Footer'
-import './Components/footer/Footer.css'
-import Header from './Components/header/Header'
-import './Components/header/Header.css'
+import Footer from './Components/footer/Footer';
+import './Components/footer/Footer.css';
+import Header from './Components/header/Header';
+import './Components/header/Header.css';
+import React, { useState, useEffect } from 'react';
+import Pagination from './Pagination';
+import './pagination.css'
+// import NavigationBar from './NavegationBar';
+// import ProductListFiltered from './ProductListFiltered';
 
 function App() {
+
   return (
     <div>
       <Header />
+      {/* <NavigationBar />
+      <ProductListFiltered/> */}
+      <Pagination  />
       <Footer />
     </div>
   );
 }
 
 export default App;
-
--- src/Components/Products.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function () {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())
-    .then(json => console.log(json))
-} 
-
-export default Products
