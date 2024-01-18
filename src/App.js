dbe432e016520a376f392d718e8b0f0ff4772e78
JeanCarlosPerez <jeancperez09@gmail.com>
2024-01-18 11:52:06 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import RoutesProject from './routes/routes';
-=======
 import './App.css';
 import Footer from './Components/footer/Footer';
 import './Components/footer/Footer.css';
@@ -11,14 +8,10 @@ import Pagination from './Pagination';
 import './pagination.css'
 // import NavigationBar from './NavegationBar';
 // import ProductListFiltered from './ProductListFiltered';
->>>>>>> 4919101612ef7219c355d004caa5be4a89283aa4
 
 function App() {
 
   return (
-<<<<<<< HEAD
-    <RoutesProject />
-=======
     <div>
       <Header />
       {/* <NavigationBar />
@@ -26,7 +19,6 @@ function App() {
       <Pagination  />
       <Footer />
     </div>
->>>>>>> 4919101612ef7219c355d004caa5be4a89283aa4
   );
 }
 
