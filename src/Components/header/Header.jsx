4ac6421ada6bdcd90a47c6ab80ca7e31e49c1920
andreagarciacedeno <andreagarciacedeno@gmail.com>
2024-01-22 09:07:56 +0100

    update fio

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -17,28 +17,6 @@ function Header({ cartItemCount }) {
     };
 
     return (
-<<<<<<< HEAD
-      <header className="header">
-        <nav className="barra">
-          <Link to="#">mataplantas.com</Link>
-          <ul className="menu">
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/Products">Productos</Link></li>
-            <li><Link to="/Register">Registro</Link></li>
-            <li><Link to="/Search">🔎</Link></li>
-            <li><Link to="/Cart"><CartBadge /></Link></li>
-          </ul>
-          <button id="toggleBtn" onClick={handleToggleMenu}>
-            <svg className="svg-icon" fill="none" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
-              {/* ... Contenido del SVG ... */}
-            </svg>
-            <span className="lableIt">Modo visual</span>
-          </button>
-          <input className="menu__btn" type="checkbox" id="menu__btn" />
-          <label className="menu__icon" htmlFor="menu__btn"><span className="navicon"></span> </label>
-        </nav>
-      </header>
-=======
         <header className="header">
             <nav className="barra">
                 <Link to="#">mataplantas.com</Link>
@@ -57,7 +35,6 @@ function Header({ cartItemCount }) {
                 <label className="menu__icon" htmlFor="menu__btn" onClick={handleToggleMenu}><span className="navicon"></span></label>
             </nav>
         </header>
->>>>>>> ba006f5882670d1a6c8c311a5a6e50b14fbd77af
     );
 }
 
