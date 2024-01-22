89805643418cd33571e2fbcf6798469d4c909668
DavidCabrerizo <visualproductor@gmail.com>
2024-01-22 15:26:11 +0100

    search bar working

--- src/Components/forms/Registercomponent.jsx
+++ src/Components/forms/Registercomponent.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import React, { useRef, useState } from "react";
-=======
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
->>>>>>> ab19eb9c512c9667a564db94ab526fde68511bfe
 
 export const Registercomponent = (props) => {
   const [email, setEmail] = useState('');
