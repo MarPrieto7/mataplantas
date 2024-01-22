1a7da9794a139a3ffcd6c5f51bf0c3e1430046b0
andreagarciacedeno <andreagarciacedeno@gmail.com>
2024-01-22 13:04:49 +0100

    update

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import InputField from "../../src/Components/contactform/InputField";
 import TextAreaField from "../../src/Components/contactform/TextAreaField";
 
@@ -7,47 +8,50 @@ const ContactForm = () => {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [note, setNote] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Formulario enviado:", { name, phone, email, note });
+    
+    // Simulación de un proceso asíncrono, por ejemplo, una solicitud HTTP
     setTimeout(() => {
       alert("Formulario de contacto enviado con éxito");
-  }, 2000);
+      
+      // Redirige a la página de inicio después de la alerta
+      navigate("/");
+    }, 0);
   };
 
   return (
-  
-
     <div className="contact-form-container">
-      
       <form className="contact-form" onSubmit={handleSubmit}>
-      <h4>Dudas o incidencias.Contáctanos</h4>
+        <h4>Dudas o incidencias. Contáctanos</h4>
         <InputField
-        label="Nombre"
-        type="text"
-        id="name"
-        value={name}
-        onChange={setName}
+          label="Nombre"
+          type="text"
+          id="name"
+          value={name}
+          onChange={setName}
         />
         <InputField
-        label="Teléfono"
-        type="tel"
-        id="phone"
-        value={phone}
-        onChange={setPhone}
+          label="Teléfono"
+          type="tel"
+          id="phone"
+          value={phone}
+          onChange={setPhone}
         />
         <InputField
-        label="Email"
-        type="email"
-        id="email"
-        value={email}
-        onChange={setEmail}
+          label="Email"
+          type="email"
+          id="email"
+          value={email}
+          onChange={setEmail}
         />
         <TextAreaField label="Nota" id="note" value={note} onChange={setNote} />
-      <button type="submit">Enviar</button>
+        <button type="submit">Enviar</button>
       </form>
-    </ div>
+    </div>
   );
 };
 
